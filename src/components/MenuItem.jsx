6bc0240168d7743a30a7884e7b18b73199cbcab7
steezy <karadimos.giannis@gmail.com>
2023-09-17 13:09:05 +0300

    added dish descriptions

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,78 +1,91 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping, faFilter } from "@fortawesome/free-solid-svg-icons";
+import Modal from "react-modal";
+import closeModal from "../images/close.svg";
 import "./MenuItem.css";
 
 const MenuItem = ({ item }) => {
-  const [showIngredients, setShowIngredients] = useState(false);
+  Modal.setAppElement("#root");
 
-  const toggleIngredients = () => {
-    setShowIngredients(!showIngredients);
-  };
-  const dietaryPreferences =
-    item.tags && item.tags.dietaryPreferences
-      ? item.tags.dietaryPreferences.join(", ")
-      : "Not specified";
-
-  const allergens =
-    item.tags && item.tags.allergens
-      ? item.tags.allergens.join(", ")
-      : "Not specified";
+  const [showModal, setShowModal] = useState(false);
+  const handleOpenModal = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
 
   return (
-    <div className="menu-item">
-      <div className="item-container relative">
-        <img
-          src={item.image}
-          alt={item.name}
-          className="item-image m-4 rounded-lg"
-        />
-        <div className="main-description absolute inset-0 flex flex-col justify-between items-center text-center">
-          <h3 className="item-name bg-opacity-50 bg-gray-900 rounded p-2 text-white text-3xl font-extrabold mt-4">
-            {item.name}
-          </h3>
-          {/*<p className="text-sm mb-2">{item.description}</p>*/}
-          <div className="flex  justify-center mb-6">
-            <p className="item-price bg-opacity-75 bg-gray-900 rounded p-2 text-white text-lg font-bold ">
-              Price: ${item.price.toFixed(2)}
-            </p>
-            <button>
-              <FontAwesomeIcon
-                className="bg-red-900 rounded ml-4 p-2"
-                icon={faCartShopping}
-                style={{ color: "#ffffff" }}
-                size="xl"
-              />
-            </button>
-          </div>
-
-          {/* <button
-          onClick={toggleIngredients}
-          type="button"
-          className="ing-button rounded-md m-1 overflow-hidden relative group cursor-pointer border-2 font-medium border-indigo-600 text-indigo-600 text-white"
-        >
-          {showIngredients ? "Hide Ingredients" : "View Ingredients"}
-        </button>
-        {showIngredients && (
-          <div>
-            <p className="font-bold">Ingredients:</p>
-            <div>
-              {item.ingredients.map((ingredient, index) => (
-                <span key={index}>
-                  {ingredient}
-                  {index < item.ingredients.length - 1 && ", "}
-                </span>
-              ))}
+    <>
+      <div className="menu-item" onClick={handleOpenModal}>
+        <div className="item-container relative">
+          <img
+            src={item.image}
+            alt={item.name}
+            className="item-image m-4 rounded-lg"
+          />
+          <div className="main-description absolute inset-0 flex flex-col justify-between items-center text-center">
+            <h3 className="item-name bg-opacity-50 bg-gray-900 rounded p-2 text-white text-3xl font-extrabold mt-4">
+              {item.name}
+            </h3>
+            <div className="flex  justify-center mb-6">
+              <p className="item-price bg-opacity-75 bg-gray-900 rounded p-2 text-white text-lg font-bold ">
+                Price: ${item.price.toFixed(2)}
+              </p>
+              <button>
+                <FontAwesomeIcon
+                  className="bg-red-500 rounded-full ml-4 p-2"
+                  icon={faCartShopping}
+                  style={{ color: "#ffffff" }}
+                  size="xl"
+                />
+              </button>
             </div>
           </div>
-        )}
-      </div>
-       <div>Dietary Preferences: {dietaryPreferences}</div>
-      <div>Allergens: {allergens}
-              */}
         </div>
       </div>
-    </div>
+      <Modal
+        isOpen={showModal}
+        onRequestClose={handleCloseModal}
+        style={{
+          content: {
+            backgroundColor: "#101010",
+            color: "#9f9f9f",
+            padding: "60px",
+            display: "flex",
+            flexDirection: "column",
+            width: "400px",
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            marginRight: "-50%",
+            transform: "translate(-50%, -50%)",
+            zIndex: "999",
+          },
+        }}
+      >
+        <img
+          src={closeModal}
+          className="closeMenu closeModal"
+          onClick={handleCloseModal}
+          alt="Close"
+        ></img>
+        <h3 className="dishTitle font-bold">{item.name}</h3>
+        <p className="dishDescription">{item.description}</p>
+        <div>
+          <p className="font-bold text-center text-white">Ingredients:</p>
+          <div>
+            {item.ingredients.map((ingredient, index) => (
+              <span key={index}>
+                {ingredient}
+                {index < item.ingredients.length - 1 && ", "}
+              </span>
+            ))}
+          </div>
+        </div>
+        <button className="rounded-full bg-blue-500 mt-4 font-bold text-white h-10">
+          Add to cart
+        </button>
+      </Modal>
+    </>
   );
 };
 
